4c278f1e473b5e0bb68687c9fb5dc95abd2f3d99
AssoDePicche <samuelprado730@gmail.com>
2023-01-09 16:32:07 -0300

    Feat: Add create-project.bat

--- cmd/bin/create-project.bat
+++ cmd/bin/create-project.bat
@@ -10,8 +10,16 @@ MD %PROJECT_NAME%
 
 CD %PROJECT_NAME%
 
+MD vendor
+
 MD src
 
 MD __tests__
+
+ECHO %PROJECT_NAME% >> README.md
+
+ECHO -e '.gitignore\n/vendor/\n/__tests__/' >> .gitignore
+
+git init
 
 code .
